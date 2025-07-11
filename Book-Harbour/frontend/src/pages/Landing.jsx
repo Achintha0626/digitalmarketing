e8fea90e8055dddc137c799661c508e5ba91dbab
Achintha0626 <achintha0626@gmail.com>
2025-07-11 11:22:48 +0530

    complete the landing page

--- Book-Harbour/frontend/src/pages/Landing.jsx
+++ Book-Harbour/frontend/src/pages/Landing.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            book <span>managing</span>app{" "}
+          </h1>
+          <p>
+            "Organize your job search like a pro! Track how many applications
+            you've sent and stay on top of your goals."
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn register-link">
+            Login/Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Landing
+export default Landing;
